634f613293bd000317bca66a1f74e671c04053de
whojayantkumar <jayant.tjr@gmail.com>
2021-06-27 22:36:40 +0530

    merge two sorted arrays

---
+++ javascript/Array/mergeSortedArrays.js
@@ -0,0 +1,30 @@
+// array1 = [0,3,4,31]
+//array2 = [4,6,30]
+//mergeSortedArray = [0,3,4,4,6,30,31]
+//Time O(n1 + n2), Space O(n1+n2)
+function mergeSortedArray(array1, array2) {
+    var mergedArray = [];
+    var i = 0;
+    var j = 0;
+    while (i < array1.length && j < array2.length) {
+        if (array1[i] <= array2[j]) {
+            mergedArray.push(array1[i]);
+            i++;
+        }
+        else {
+            mergedArray.push(array2[j]);
+            j++;
+        }
+    }
+    while (i < array1.length) {
+        mergedArray.push(array1[i]);
+        i++;
+    }
+    while (j < array2.length) {
+        mergedArray.push(array2[j]);
+        j++;
+    }
+    return mergedArray;
+}
+console.log(mergeSortedArray([0, 3, 4, 31], [4, 6, 30]));
+console.log(mergeSortedArray([0, 3, 4, 31], [4, 6, 30, 31, 62]));
